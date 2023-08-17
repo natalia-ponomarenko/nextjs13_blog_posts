905db65f60a3dd5a61823b8f3e4c541e5c0562fd
natalia-ponomarenko <virus.zabutiya@gmail.com>
2023-08-17 15:18:14 +0300

    another revalidate to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import classNames from 'classnames';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
+export const revalidate = 0;
 
 export const metadata = {
   title: 'Next.js 13 Blog/News',
